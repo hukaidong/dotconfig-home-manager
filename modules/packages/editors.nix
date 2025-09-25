33330cafb132c0e4d17eb8b63d4fe4e96c7f554c
Kaidong Hu <hukaidonghkd@gmail.com>
2025-09-25 17:18:48 -0400

    refactor(nix): optimize LaTeX installation for Emacs org-mode

--- modules/packages/editors.nix
+++ modules/packages/editors.nix
@@ -1,8 +1,26 @@
 { pkgs, ... }:
-
+# Minimum set of packages for Emacs org mode, see https://wiki.nixos.org/wiki/TexLive
+let
+  tex = (
+    pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        scheme-medium
+        dvisvgm
+        dvipng # for preview and export as html
+        wrapfig
+        amsmath
+        ulem
+        hyperref
+        capt-of
+        ;
+      #(setq org-latex-compiler "lualatex")
+      #(setq org-preview-latex-default-process 'dvisvgm)
+    }
+  );
+in
 {
   home.packages = with pkgs; [
     # emacs - provides latex preview
-    texliveMedium
+    tex
   ];
 }
